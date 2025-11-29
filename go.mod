42cff8da8e53933e311d21a69b476a47f0b68405
AN Long <aisk1988@gmail.com>
2025-11-29 21:01:17 +0900

    🚧 Make the package buildable

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/aisk/route122
 
-go 1.23.5
+go 1.22
+
+require golang.org/x/net v0.35.0
+
+require golang.org/x/text v0.22.0 // indirect
---
+++ support.go
@@ -0,0 +1,57 @@
+// Vendored from go1.25.4's net/http package
+
+package route122
+
+import (
+	"path"
+	"strings"
+
+	"golang.org/x/net/http/httpguts"
+)
+
+type Handler any
+
+// cleanPath returns the canonical path for p, eliminating . and .. elements.
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		// Fast path for common case of p being the string we want:
+		if len(p) == len(np)+1 && strings.HasPrefix(p, np) {
+			np = p
+		} else {
+			np += "/"
+		}
+	}
+	return np
+}
+
+// isToken reports whether v is a valid token (https://www.rfc-editor.org/rfc/rfc2616#section-2.2).
+func isToken(v string) bool {
+	// For historical reasons, this function is called ValidHeaderFieldName (see issue #67031).
+	return httpguts.ValidHeaderFieldName(v)
+}
+
+func validMethod(method string) bool {
+	/*
+	     Method         = "OPTIONS"                ; Section 9.2
+	                    | "GET"                    ; Section 9.3
+	                    | "HEAD"                   ; Section 9.4
+	                    | "POST"                   ; Section 9.5
+	                    | "PUT"                    ; Section 9.6
+	                    | "DELETE"                 ; Section 9.7
+	                    | "TRACE"                  ; Section 9.8
+	                    | "CONNECT"                ; Section 9.9
+	                    | extension-method
+	   extension-method = token
+	     token          = 1*<any CHAR except CTLs or separators>
+	*/
+	return isToken(method)
+}
